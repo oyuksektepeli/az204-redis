4984c544e6de9e97b831813c34b5a4ee126a0ace
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2021-06-18 12:39:41 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace az204_redis
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            string connectionString = "az204redis01.redis.cache.windows.net:6380,password=WRolrL5uiM3Red7Np29nsr6A6P4LdnzfYnosFf098WE=,ssl=True,abortConnect=False";
+            using(var cache = ConnectionMultiplexer.Connect(connectionString))
+            {
+                IDatabase db = cache.GetDatabase();
+                bool setValue = await db.StringSetAsync("test:key", "100");
+                Console.WriteLine($"SET:{setValue}");
+
+                string getValue = await db.StringGetAsync("test:key");
+                Console.WriteLine($"GET: {getValue}");
+            }
+        }
+    }
+}
